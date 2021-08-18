4dff5fa09291747a230603cf84d08e038590d644
Brian Summerfield <brianjsummerfield@gmail.com>
2021-08-18 15:14:27 -0500

    controller inititalized

---
+++ controller/price_calculator.rb
@@ -0,0 +1,12 @@
+require '../models/pricing_table'
+require '../models/cart'
+require '../views/view_cart'
+
+items = ViewCart.get_items
+cart = Cart.new
+cart.parse(items)
+cart.total
+ViewCart.render(cart)
+
+
+
