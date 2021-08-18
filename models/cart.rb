d3cfb3872cac381de304130aaf0fd0bbe242ed9a
Brian Summerfield <brianjsummerfield@gmail.com>
2021-08-18 14:06:31 -0500

    add cart total/discount methods

--- models/cart.rb
+++ models/cart.rb
@@ -1,17 +1,17 @@
 require "./pricing_table"
 
 class Cart
-  attr_accessor :items, :total, :savings
+  attr_accessor :items
 
   def initialize(params = {})
     @items = params.fetch(:items, Hash.new)
-    @total = params.fetch(:total, nil)
-    @savings = params.fetch(:savings, nil)
   end
 
   def create(item)
     items[item] = {
-      :quantity => 1
+      :quantity => 1,
+      :total => nil,
+      :discount => 0
     }
   end
 
@@ -25,16 +25,38 @@ class Cart
   end
 
   def parse(string)
-    string.downcase.gsub(' ', '').split(',').each do |item|
+    string.downcase.gsub(" ", "").split(",").each do |item|
       increment_or_create(item)
+    end
+  end
+
+  def set_item_total(item)
+    this_item = items[item]
+    this_item[:total] = (PRICING_TABLE[item][:price] * this_item[:quantity]) - this_item[:discount]
+  end
+
+  def set_item_discount(item)
+    this_item = items[item]
+    table_item = PRICING_TABLE[item]
+    this_item[:discount] = table_item[:sale_price] * (this_item[:quantity] / table_item[:sale_quantity])
+  end
+
+  def total
+    items.each do |item, qty|
+      set_item_discount(item) if PRICING_TABLE[item][:sale] == true
+      set_item_total(item)
     end
   end
 
 end
 
-c = Cart.new(:items => {'milk' => {:quantity => 3}})
-c.parse('mIlk , apple')
-c.parse('apple')
+# cart = Cart.new
+# cart.parse("milk,milk, bread,banana,bread,bread,bread,milk,apple")
+# cart.total
+# cart.items.each do |k,v|
+#   p k
+#   p cart.items[k][:total].to_s("F")
+#   p "**"
+# end
 
-# TODO add logic to total using pricing table
 
