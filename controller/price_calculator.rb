c59752356e2abf890649e584d5eb78d40f184c69
Brian Summerfield <brianjsummerfield@gmail.com>
2021-08-19 17:18:24 -0500

    view for table/header/footer added

--- controller/price_calculator.rb
+++ controller/price_calculator.rb
@@ -1,9 +1,9 @@
 require_relative '../models/cart.rb'
 require_relative '../views/view_cart.rb'
 
-items = ViewCart.get_items
+terminal_view = ViewCart.new
+terminal_view.prompt_items
+items = gets.chomp
 cart = Cart.new
 cart.parse(items)
-p cart
-cart.total
-ViewCart.render(cart)
+terminal_view.render_cart(cart)
--- views/view_cart.rb
+++ views/view_cart.rb
@@ -1,40 +1,15 @@
-# class ViewCart
-#   def self.render(cart)
-#     print_headers
-#     total = 0
-#     savings = 0
-#     cart.items.each do |key,value|
-#       key_length = key.length
-#       quantity_length = value[:quantity].to_s.length
-#       puts key.capitalize() + " " * (12 - key_length) + value[:quantity].to_s + " " * (12 - quantity_length) + "$" + value[:total].to_s("F")
-#       total += value[:total]
-#       savings += value[:discount]
-#     end
-#     print_footer(total, savings)
-#   end
-
-#   def self.print_headers
-#     puts "\n"
-#     puts "Item      Quantity      Price"
-#     puts "------------------------------------"
-#   end
-
-#   def self.print_footer(total,savings)
-#     puts "\n"
-#     puts "Total price : $#{total.to_s("F")}"
-#     puts "You saved $#{savings.to_s("F")} today."
-#   end
-
-#   def self.get_items
-#     puts "Please enter all the items purchased separated by a comma"
-#     return gets.chomp
-#   end
-# end
-
 class ViewCart
   def render_cart(cart)
     render(header)
+    render(item_list(cart))
     render(footer(cart.get_total_cost, cart.get_total_discount))
+  end
+
+  def item_list(cart, table = [])
+    cart.items.each do |key, value|
+      table << key.to_s.capitalize() + " " * (12 - (key.to_s).length) + value[:quantity].to_s + " " * (12 - (value[:quantity].to_s).length) + "$#{cart.get_item_total(key).to_s("F")}"
+    end
+    table.join("\n")
   end
 
   def render(template)
@@ -47,6 +22,7 @@ class ViewCart
 
   def header
     [
+      "",
       "Item      Quantity      Price",
       "------------------------------------"
     ].join("\n")
@@ -54,6 +30,7 @@ class ViewCart
 
   def footer(total, discount)
     [
+      "",
       "Total price : $#{total.to_s("F")}",
       "You saved $#{discount.to_s("F")}"
     ].join("\n")
